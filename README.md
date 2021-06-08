8488c3227ddde7c30b7e3e0063514c76729b756f
Shuntaro Yada <contact@shuntaroy.com>
2021-06-08 16:08:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# MedNER-J Radiorep
+
+ユーザが入力した医療文書に対し，PRISMアノテーションを付与した結果を表示する．
+
+Clinical NER result viewer based on MedNER-J's radiorep model.
+
+## Requirements
+
+- Python 3.8 (could work with 3.6+ but not tested)
+- mojimoji
+- [MedNER-J](https://github.com/sociocom/MedNER-J.git)
+- Flask
+
+## Development
+
+Developed by [Shuntaro Yada](https://shuntaroy.com) in [Social Computing Lab.](https://sociocom.naist.jp) at [NAIST](https://www.naist.jp).
+
+## Licence
+
+To be announced.
---
+++ app.py
@@ -0,0 +1,136 @@
+import json
+import re
+import xml.etree.ElementTree as ET
+
+import mojimoji
+from flask import (Flask, Markup, redirect, render_template, request,
+                   send_from_directory, session)
+from medner_j import Ner
+
+model = Ner.from_pretrained(model_name="radiology", normalizer="dict")
+
+# to html
+TAGNAMES = {
+    "d": "disease",
+    "a": "anatomical",
+    "f": "feature",
+    "c": "change",
+    "TIMEX3": "TIMEX3",
+    "t-test": "testtest",
+    "t-key": "testkey",
+    "t-val": "testval",
+    "m-key": "medkey",
+    "m-val": "medval",
+    "cc": "cc",
+    "r": "remedy",
+    "p": "pending",
+}
+
+app = Flask(__name__)
+app.secret_key = "0a1f4c96f1c6e4e32fdf25b6e8516f77"
+
+
+def e_xml2html(entity):
+    attrv = entity.attrib.values()
+    if len(attrv) == 1:
+        entity.attrib = {"class": f"{TAGNAMES[entity.tag]}-{list(attrv)[0]}"}
+    elif len(attrv) == 0:
+        entity.attrib = {"class": f"{TAGNAMES[entity.tag]}"}
+    else:
+        entity.attrib = {"class": f"{TAGNAMES[entity.tag]} {' '.join(attrv)}"}
+    entity.tag = "span"
+
+
+def xml2html(doc):
+    if isinstance(doc, str):
+        root = ET.fromstring(doc)
+    else:
+        root = doc
+    for entity in root.iter():
+        if entity.tag == "root":
+            continue
+        e_xml2html(entity)
+    return ET.tostring(root, encoding="unicode", method="html")
+
+
+def mednerj2xml(analysed_text):
+    xmldoc = f"<root>{analysed_text}</root>"
+    root = ET.fromstring(xmldoc)
+    for entity in root.iter():
+        if entity.tag == "root":
+            continue
+        if "value" in entity.attrib:
+            del entity.attrib["value"]
+
+        if entity.tag == "TIMEX3DATE":
+            entity.tag = "TIMEX3"
+            entity.attrib["type"] = "DATE"
+        elif entity.tag == "TIMEX3CC":
+            entity.tag = "TIMEX3"
+            entity.attrib["type"] = "CC"
+        elif entity.tag == "d":
+            entity.tag = "d"
+        elif entity.tag == "dpositive":
+            entity.tag = "d"
+            entity.attrib["certainty"] = "positive"
+        elif entity.tag == "dnegative":
+            entity.tag = "d"
+            entity.attrib["certainty"] = "negative"
+        elif entity.tag == "dsuspicious":
+            entity.tag = "d"
+            entity.attrib["certainty"] = "suspicious"
+        elif entity.tag == "mkeyexecuted":
+            entity.tag = "m-key"
+            entity.attrib["state"] = "executed"
+        elif entity.tag == "mvalexecuted":
+            entity.tag = "m-val"
+            entity.attrib["state"] = "executed"
+        elif entity.tag == "rexecuted":
+            entity.tag = "r"
+            entity.attrib["state"] = "executed"
+        elif entity.tag == "ttestexecuted":
+            entity.tag = "t-test"
+            entity.attrib["state"] = "executed"
+        elif entity.tag == "ttestother":
+            entity.tag = "t-test"
+            entity.attrib["state"] = "other"
+        elif entity.tag == "ccother":
+            entity.tag = "cc"
+            entity.attrib["state"] = "other"
+
+    return root
+
+
+def analyse(text):
+    text = mojimoji.han_to_zen(text)
+    analysed_text = model.predict([text])
+    xml = mednerj2xml(analysed_text[0])
+    return xml
+
+
+# form input
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        if "html" in session:
+            session.pop("html", None)
+        # return send_from_directory("static", "index.html")
+        return render_template("index.html")
+    else:
+        input_rr = request.form.get("radiorep")
+        if input_rr:
+            analysed_xml = analyse(request.form["radiorep"].strip())
+            radiorep_ner = Markup(xml2html(analysed_xml))
+            session["html"] = radiorep_ner
+            return render_template(
+                "index.html",
+                radiorep=input_rr,
+                radiorep_ner=radiorep_ner,
+            )
+        else:
+            return render_template("index.html")
+        # return render_template("index.html", radiorep_ner=r"<span class=\"disease\">ERROR: 解析に失敗しました……</span>")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
